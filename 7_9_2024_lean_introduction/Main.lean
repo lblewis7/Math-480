b7b22354c1f677854c1fe4aaabcaab1965692037
lblewis7 <lblewis7@uw.edu>
2024-07-10 15:36:16 -0700

    [SUBMISSION]

--- 7_9_2024_lean_introduction/Main.lean
+++ 7_9_2024_lean_introduction/Main.lean
@@ -31,19 +31,20 @@ Tactics cheatsheet:
   - `assoc` and `comm` for associativity and commutativity
 -/
 
-theorem linear_formula (a : ℝ) (b: ℝ) (x: ℝ) (ha_ne_zero : a ≠ 0) : a*x+b = 0 ↔ x = -b/a := by
+theorem linear_formula (a : ℝ) (b: ℝ) (x: ℝ) (aNonzero : a ≠ 0) : a*x+b = 0 ↔ x = -b/a := by
   apply Iff.intro
   case mp =>
     intro h
     calc x = x - 0 := by simp
          _ = x - 0/a := by simp
          _ = x - (a*x+b)/a := by rw [h]
-         _ = x - (a/a)*xsorry - b/a := by ring
-         _ = x - 1*x - b/a := by simp [ha_ne_zero]
+         _ = x - a*x/a - b/a := by ring
+         _ = x - x - b/a := by simp [aNonzero]
          _ = -b/a := by ring
   case mpr =>
     intro h
-    calc a*x+b = a*(-b/a) + b := by rw [h]
-             _ = (a/a) * -b + b := by ring
-             _ = 1 * -b + b := by simp [ha_ne_zero]
-             _ = 0 := by ring
+    calc a*x+b = a*x + b*(a/a) := by simp [aNonzero]
+             _ = a*x + a*(b/a) := by ring
+             _ = a*x - a*(-b/a) := by ring
+             _ = a*x - a*x := by rw [h]
+             _ = 0 := by simp
